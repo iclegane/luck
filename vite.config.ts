dd0260a73b5efd447e050203d88ecc12a8bb2183
Viktor <vvs-PathOfExile@yandex.ru>
2023-11-05 14:45:22 +0300

    init

---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite';
+import { checker } from 'vite-plugin-checker';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [
+        react(),
+        checker({
+            typescript: true,
+        }),
+    ],
+    root: './src',
+    base: '',
+    server: {
+        port: 8080,
+    },
+});
